0181c6a40cc1d33b1d9916cedc5a5fe8ba0bcd90
Ross Johnson <57044245+RossJ24@users.noreply.github.com>
2020-07-01 14:50:25 -0400

    Update RoomState.java

--- src/main/java/com/google/sps/data/RoomState.java
+++ src/main/java/com/google/sps/data/RoomState.java
@@ -1,31 +1,36 @@
 package com.google.sps.data;
 
+import com.google.appengine.api.datastore.EmbeddedEntity;
+
 public class RoomState {
 
-    public static DataUtil.video_state currentState;
-    public static long currentVideoTimestamp;
-    public static int currentVideo;
-    public static int messageCount;
-
+    private VideoState currentState;
+    private long currentVideoTimestamp;
+    private int currentVideo;
     //Default RoomState constructor
     public RoomState() {
-        this.currentState = DataUtil.video_state.UNSTARTED;
+        this.currentState = VideoState.UNSTARTED;
         this.currentVideoTimestamp = 0;
         this.currentVideo = 0;
-        this.messageCount = 0;
     }
     /**
       * RoomState constructor
       * @param state an enum element representing the YT player state
       * @param videoTimeStamp a long representing the seek position of the current video
       * @param currentVideo an int representing the current video's index in the URL list
-      * @param messageCount an int representing the number of messages currently in the message list 
       * @return a new RoomState object
       */
-    public RoomState(DataUtil.video_state state, long videoTimeStamp, int currentVideo, int messageCount) {
+    public RoomState(VideoState state, long videoTimeStamp, int currentVideo, int messageCount) {
         this.currentState = state;
         this.currentVideoTimestamp = videoTimeStamp;
         this.currentVideo = currentVideo;
-        this.messageCount = messageCount;
+    }
+
+    public EmbeddedEntity toEmbeddedEntity(){
+        EmbeddedEntity stateEntity = new EmbeddedEntity();
+        stateEntity.setProperty("currentState", this.currentState.getValue());
+        stateEntity.setProperty("currentVideoTimestamp", this.currentVideoTimestamp);
+        stateEntity.setProperty("currentVideo", this.currentVideo);
+        return stateEntity;
     }
 }
