3e2ca8d2536cc26df421e2a9fc5055d39dbf87e8
Ross Johnson <57044245+RossJ24@users.noreply.github.com>
2020-07-01 14:55:15 -0400

    Update CreateRoomServlet.java

--- src/main/java/com/google/sps/servlets/CreateRoomServlet.java
+++ src/main/java/com/google/sps/servlets/CreateRoomServlet.java
@@ -8,12 +8,14 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
+import java.util.Queue;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.sps.data.PrivateRoom;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,9 +27,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import com.google.gson.JsonArray;
 import java.util.Arrays;
+import com.google.appengine.api.datastore.EmbeddedEntity;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.DatastoreFailureException;
+import com.google.appengine.api.datastore.QueryResultList;
 
 @WebServlet("/create-room")
 public final class CreateRoomServlet extends HttpServlet {
+    public static final DatastoreService DATASTORE = DatastoreServiceFactory.getDatastoreService();
 
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
@@ -38,20 +46,29 @@ public final class CreateRoomServlet extends HttpServlet {
         //Requests playlist information from YT api and transforms playlist url to video url list
         String playlistUrl = req.getParameter("PlaylistUrl");
         String playlistId = playlistUrl.substring(playlistUrl.indexOf(ServletUtil.PLAYLIST_QUERY_PARAMETER)+5);
-
-        ArrayList<String> videoUrls = playlistIdToUrls(playlistId);
+        
+        Queue<String> videoUrls = playlistIdToUrls(playlistId);
         Room newRoom = new Room(members, videoUrls);
 
-        //TODO(rossjohnson): Find an efficient way to store this in datastore since it has collections of objects in it
+        Entity roomEntity = newRoom.toEntity();
+        
+        try{
+            DATASTORE.put(roomEntity);
+        } 
+        catch(DatastoreFailureException e){
+            System.out.println(e.toString());
+        }
+        
+
     }
     /**
       * Communicates with the Youtube Data API to get playlistItem information
       * @param playlistId the string representing the playlistId
       * @return an arraylist of video Urls (limit is 15)
       */
-    ArrayList<String> playlistIdToUrls(String playlistId){
+    Queue<String> playlistIdToUrls(String playlistId) throws IOException {
         //Connect to the YouTube Data API
-        URL url = new URL("https://www.googleapis.com/youtube/v3/playlistItems?key="+ServletUtil.DATA_API_KEY+"&part=contentDetails&playlistId="+playlistID);
+        URL url = new URL("https://www.googleapis.com/youtube/v3/playlistItems?key="+ServletUtil.DATA_API_KEY+"&part=contentDetails&playlistId="+playlistId);
         HttpURLConnection YTDataCon = (HttpURLConnection) url.openConnection();
         YTDataCon.setRequestMethod("GET");
         //Read the response
@@ -66,12 +83,12 @@ public final class CreateRoomServlet extends HttpServlet {
         //Parse json for the specific data that is necessary
         JsonObject obj = ServletUtil.PARSER.fromJson(content.toString(), JsonObject.class);
         JsonArray VideoInformation = obj.getAsJsonArray("items");
-        ArrayList<String> videoUrls =  new ArrayList<String>();
+        Queue<String> videoUrls = new LinkedList<String>();
         //Create urls from video IDs
         for(int i = 0; i < VideoInformation.size() && i < 15; ++i ) {
             String videoid = VideoInformation.get(i).getAsJsonObject().getAsJsonObject("contentDetails").get("videoId").getAsString();
             videoUrls.add(ServletUtil.YT_BASE_URL + videoid);
         }
-        return videoUrls
+        return videoUrls;
     }
 }
