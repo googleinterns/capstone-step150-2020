f7d10d66a5999a3c2912bd14482e2fbfc4245cac
Ross Johnson <57044245+RossJ24@users.noreply.github.com>
2020-07-01 14:45:56 -0400

    Update Room.java

--- src/main/java/com/google/sps/data/Room.java
+++ src/main/java/com/google/sps/data/Room.java
@@ -1,20 +1,21 @@
-
 package com.google.sps.data;
 
+import com.google.appengine.api.datastore.Entity;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.List;
 import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
+import com.google.appengine.api.datastore.EmbeddedEntity;
 
 public class Room {
 
     private List<String> members;
     private LinkedList<Message> messages;
-    private ArrayList<String> video_urls;
+    private Queue<String> videoUrls;
     private RoomState state;
 
     /**
@@ -23,22 +24,38 @@ public class Room {
       * @param urls an arraylist of the video urls of the initial Room playlist
       * @return a new Room object
       */
-    public Room(List<String> members, ArrayList<String> urls){
+    public Room(List<String> members, Queue<String> urls){
         this.messages = new LinkedList<Message>();
         this.state = new RoomState();
         this.members = members;
-        this.video_urls = urls;
+        this.videoUrls = urls;
     }
     //Returns the Room's members
     public List<String> getMembers() {
         return this.members;
     }
     //Returns the Room's video url list
-    public ArrayList<String> getUrls() {
-        return this.video_urls;
+    public Queue<String> getUrls() {
+        return this.videoUrls;
     }
     //Returns the Room's message list
     public LinkedList<Message> getMessages(){
         return this.messages;
+    }
+    public LinkedList<EmbeddedEntity> getMessagesAsEntities() {
+        LinkedList<EmbeddedEntity> ret = new LinkedList<EmbeddedEntity>();
+        for(Message m : this.messages){
+            ret.add(m.toEmbeddedEntity());
+        }
+        return ret;
+    }
+    public Entity toEntity(){
+        Entity newRoom = new Entity("Room");
+        newRoom.setProperty("members", this.members);
+        newRoom.setProperty("videoUrls", this.videoUrls);
+        List<EmbeddedEntity> messageEntities = getMessagesAsEntities();
+        newRoom.setProperty("messages", messageEntities);
+        newRoom.setProperty("roomState", this.state.toEmbeddedEntity());
+        return newRoom;
     }
 }
