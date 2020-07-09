c0777d61e2d8f4d19e0516a30ebf8c43ce04e7c0
Jalyn Derry <jalynderry@google.com>
2020-07-09 18:47:22 +0000

    Load playlist on ready

--- src/main/webapp/scripts/private-room-script.js
+++ src/main/webapp/scripts/private-room-script.js
@@ -1,12 +1,19 @@
+var roomId;
+var playlistUrls;
+var playlistIds;
+var player;
+
 /**
 * Calls the three functions associated with loading the room's iframe
  */
-async function loadIframe(){
+async function loadPlayerDiv(){
+    loadVideo();
 	// Get the room id from the private room's url
 	await getRoomId(window.location.href);
-	await fetchPrivateRoomVideo(window.roomId);
-	var iframeTag = document.getElementById("video-tag");
-	iframeTag.src = window.roomVideoUrl;
+	await fetchPrivateRoomVideo(roomId);
+	//var iframeTag = document.getElementById("video-tag");
+	//iframeTag.src = window.roomVideoUrl;
+    //loadRoomPlaylist();
 }
 
 /**
@@ -21,26 +28,26 @@ function getRoomId(url) {
     // Create a temp array of len=2, 
 	// holds 'roomId=' in [0] and room id in [1]
 	var tempArrayForRoomId = query.split('=');
-	var roomId = tempArrayForRoomId[1];
-	window.roomId = roomId;
+	var currentRoomId = tempArrayForRoomId[1];
+	roomId = currentRoomId;
     return roomId;
 }
 
 /**
-* Take in currentRoomId and return the Video Url associated with that room ID
+* Take in currentRoomId and create a global array of youtube video urls and 
+* a global array of the video ids of the playlist
 * @param {currentRoomId} String Holds the room Id of the the user's room
 * @return {roomVideoUrl} The Url of the video to be displayed for the room
  */
 async function fetchPrivateRoomVideo(currentRoomId) {
-    console.log("i am in the fetch private room function");
-    // Check that the current room id exits, 
-    // then return video url associated with that id
+    // Check that the current room id exits, then return playlist of given room
     let roomPromise = await fetch('/verify-room?roomId='+currentRoomId);
+    // fetch the json-version of the urls for all the youtube videos
     let roomVideoUrl = await roomPromise.json();
-    var playlistUrls = parseJsonOfVideos(roomVideoUrl);
-    window.roomVideoUrl = playlistUrls[0];
-    extractVideoIds(playlistUrls);
-    console.log("in the fetch function " + window.roomVideoUrl);
+    // create an array of all the YT videos' urls
+    playlistUrls = parseJsonOfVideos(roomVideoUrl);
+    // window.roomVideoUrl = playlistUrls[0];
+    playlistIds = extractVideoIds(playlistUrls);
 }
 
 function parseJsonOfVideos(jsonOfVideos){
@@ -50,15 +57,20 @@ function parseJsonOfVideos(jsonOfVideos){
 	}
 	return playlistUrls;
 }
+
 var YT_BASE_URL = "https://www.youtube.com/embed/";
 function extractVideoIds(playlistUrls){
-    var playlistIds = [];
+    var currentPlaylistIds = [];
     for(i = 0; i < playlistUrls.length; i++) {
         var currentUrl = playlistUrls[i];
         var currentRoomId = currentUrl.substring(YT_BASE_URL.length);
-        playlistIds.push(currentRoomId);
+        currentPlaylistIds.push(currentRoomId);
     }
-    window.playlistIds = playlistIds;
+    return currentPlaylistIds;
+}
+
+function loadRoomPlaylist(){
+    player.loadPlaylist({playlist: playlistIds});
 }
 
 // This code loads the IFrame Player API code asynchronously.
@@ -71,40 +83,62 @@ function loadVideo(){
 
 // This function creates an <iframe> (and YouTube player)
 // after the API code downloads.
-var player;
 function onYouTubeIframeAPIReady() {
-	player = new YT.Player('player', {
-		events: {
-			'onReady': onPlayerReady,
-			'onStateChange': onPlayerStateChange
-		}
-	});
+    player = new YT.Player('player-div', 
+    {
+        playerVars: {
+            autoplay: 0,      // Don't autoplay the initial video
+            rel: 0,           //  Don’t show related videos
+            theme: "light",   // Use a light player instead of a dark one
+            controls: 1,      // Show player controls
+            showinfo: 0,      // Don’t show title or loader
+            modestbranding: 1 // No You Tube logo on control bar
+        },
+        events: {               
+            // Callback when onReady fires
+            onReady: onPlayerReady,
+            // Callback when onStateChange fires
+            onStateChange: onStateChange,
+        }
+    });
 }
 
 // The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-    document.getElementById('player').style.borderColor = '#FF6D00';
-}
-
-/**
-* The API calls this function when the player's state changes.
-* The function indicates that when playing a video (state=1),
-* the player should play for six seconds and then stop.
-*/ 
-var done = false;
-function onPlayerStateChange(event) {
-	if (event.data == YT.PlayerState.PLAYING && !done) {
-		setTimeout(stopVideo, 6000);
-		done = true;
-	}
+    document.getElementById('player-div').style.borderColor = '#FF6D00';
+    loadRoomPlaylist();
 }
 
 function stopVideo() {
 	player.stopVideo();
 }
 
-function cuePlaylist(){
-    player.cuePlaylist()
+// Log state changes
+function onStateChange(event) {
+    var state = "undefiend";
+    switch (event.data) {
+        case YT.PlayerState.UNSTARTED:
+            state= "unstarted";
+            break;
+        case YT.PlayerState.ENDED:
+            state = "ended";
+            break;
+        case YT.PlayerState.PLAYING:
+            state = "playing";
+            break;
+        case YT.PlayerState.PAUSED:
+            state = "paused";
+            break;
+        case YT.PlayerState.BUFFERING:
+            state = "buffering";
+            break;
+        case YT.PlayerState.CUED:
+            state = "video cued";
+            break;
+        default:
+            state = "unknown (" + event.data + ")";
+    }
+    console.log('onStateChange: ' + state);
 }
 
 async function displayChat() {
