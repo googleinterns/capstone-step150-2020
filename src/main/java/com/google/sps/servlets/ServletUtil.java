f9cf0d66fb3963eaa4bd8c73defc09d897e31bca
Jalyn Derry <jalynderry@google.com>
2020-07-13 15:41:27 +0000

    Add other final variables to servletutil file

--- src/main/java/com/google/sps/servlets/ServletUtil.java
+++ src/main/java/com/google/sps/servlets/ServletUtil.java
@@ -1,7 +1,18 @@
 package com.google.sps.servlets;
 import com.google.gson.Gson;
 
-// Util File for Servlet that defines the path for certain HTML files
+// Util File for Servlet that defines final entities for youtube party
 public class ServletUtil {
+    public static final Gson PARSER = new Gson();
+    public static enum video_state {
+        UNSTARTED(-1), ENDED(0), PLAYING(1), PAUSED(2), BUFFERING(3), CUED(5);
+        private int value;
+        private video_state(int value){
+            this.value = value;
+        }
+    }
+    public static final String DATA_API_KEY = "AIzaSyA6NypfS8qTc_1O73fx2ZP6JwMZdB_FSas";
+    public static final String YT_BASE_URL = "https://www.youtube.com/embed/";
+    public static final String PLAYLIST_QUERY_PARAMETER = "list=";
     public static final String PRIVATE_ROOM_PATH = "/views/private-room.html";
 }
