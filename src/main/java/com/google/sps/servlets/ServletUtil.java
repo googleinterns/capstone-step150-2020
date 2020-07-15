51e8640dbfc0c1e07a58e0bb0a7b34b1a15ea26e
Jalyn Derry <jalynderry@google.com>
2020-07-15 17:35:11 +0000

    Update html and servlet util file

--- src/main/java/com/google/sps/servlets/ServletUtil.java
+++ src/main/java/com/google/sps/servlets/ServletUtil.java
@@ -3,5 +3,11 @@ import com.google.gson.Gson;
 
 // Util File for Servlet that defines the path for certain HTML files
 public class ServletUtil {
+    public static final Gson PARSER = new Gson();
+    public static final String DATA_API_KEY = "AIzaSyA6NypfS8qTc_1O73fx2ZP6JwMZdB_FSas";
+    public static final String YT_BASE_URL = "https://www.youtube.com/embed/";
+    public static final String PLAYLIST_QUERY_PARAMETER = "list=";
     public static final String PRIVATE_ROOM_PATH = "/views/private-room.html";
+    public static final String ROOM_ENTITY = "Room";
+    public static String INPUTTED_ID_TAG = "roomId";
 }
