4553cee9d3095243592e5c573d5b8238bf02ff80
Jalyn Derry <jalynderry@google.com>
2020-07-22 17:34:44 +0000

    Fix Killian requested changes

--- src/main/webapp/scripts/redirect-page-script.js
+++ src/main/webapp/scripts/redirect-page-script.js
@@ -1,10 +1,9 @@
-var roomId;
 const privateRoomPath = "/views/private-room.html?id=";
 const joinRoomPath = "/views/join-room.html";
 
 // Send inputted room Id to VerifyRoomServlet them redirect them to proper page
 async function verifyRoom() {
-    roomId = getRoomId(window.location.href);
+    var roomId = getRoomId(window.location.href);
      // Check that the current room id exits, then return playlist of given room
     let roomPromise = await fetch('/verify-room?roomId='+roomId);
     // fetch the json-version of the urls for all the youtube videos
