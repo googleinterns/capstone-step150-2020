1703f7bf726a8604468594ec2fdc78efbc22f662
Ross Johnson <Rossjohnsonpro@outlook.com>
2020-07-28 19:24:37 +0000

    Add changes to test structure

--- src/main/java/com/google/sps/servlets/AuthServlet.java
+++ src/main/java/com/google/sps/servlets/AuthServlet.java
@@ -13,12 +13,11 @@ import com.google.appengine.api.users.UserServiceFactory;
 @WebServlet("/auth")
 public class AuthServlet extends HttpServlet{
     private static final String REDIRECT_URL_PARAMETER = "redirectUrl";
-    private static final String URL_TO_REDIRECT_AFTER_LOG_OUT = "/";
     
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
-        Boolean isUserLoggedIn = isUserLoggedIn(ServletUtil.USER_SERVICE);
+        Boolean isUserLoggedIn = ServletUtil.USER_SERVICE.isUserLoggedIn();
         String urlToRedirectToAfterUserLogsIn = req.getParameter(REDIRECT_URL_PARAMETER);
         String responseBody = getResponseBody(isUserLoggedIn, urlToRedirectToAfterUserLogsIn, ServletUtil.USER_SERVICE);
         if(isUserLoggedIn){
@@ -31,18 +30,15 @@ public class AuthServlet extends HttpServlet{
         }
     }
 
-    public static Boolean isUserLoggedIn(UserService userService){
-        return userService.isUserLoggedIn();
-    }
 
     public static String getResponseBody(Boolean loggedin, String redirectUrl, UserService userService){
         if(loggedin) return userService.getCurrentUser().getEmail();
 
-        String loginUrl = userService.createLoginURL(redirectUrl);
+        String loginUrl = ServletUtil.USER_SERVICE.createLoginURL(redirectUrl);
         StringBuilder builder = new StringBuilder();
-        builder.add("<center><a href=\"");
-        builder.add(loginUrl);
-        builder.add("\"><button style=\"background-color: cyan; border-radius:12px;\"><span style=\"color:white;\">Login</span></button/></a></center>");
+        builder.append("<center><a href=\"");
+        builder.append(loginUrl);
+        builder.append("\"><button style=\"background-color: cyan; border-radius:12px;\"><span style=\"color:white;\">Login</span></button/></a></center>");
         return builder.toString();
     }
 }
--- src/test/java/com/google/sps/AuthenticationTest.java
+++ src/test/java/com/google/sps/AuthenticationTest.java
@@ -2,6 +2,7 @@ package com.google.sps;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mockito;
@@ -12,42 +13,71 @@ import com.google.appengine.api.users.User;
 import java.util.*;
 import com.google.sps.data.*;
 import com.google.sps.servlets.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.StringWriter;
+import java.io.PrintWriter;
 
 @RunWith(JUnit4.class)
 public final class AuthenticationTest {
+    private static final String REDIRECT_URL_PARAMETER = "redirectUrl";
+    static HttpServletRequest req;
+    static HttpServletResponse res;
+    StringWriter stringWriter;
+    @Before
+    public void setup() throws Exception {
+        req = Mockito.mock(HttpServletRequest.class);
+        when(req.getParameter(REDIRECT_URL_PARAMETER)).thenReturn("/redirect");
 
+        res = Mockito.mock(HttpServletResponse.class);
+        stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(res.getWriter()).thenReturn(writer);
+    }
     @Test
-    public void UserServiceTestUserIsLoggedIn(){
+    public void UserServiceTestUserIsLoggedIn() throws Exception {
         //Setup
         UserService UserServiceMock = Mockito.mock(UserService.class);
         when(UserServiceMock.isUserLoggedIn()).thenReturn(true);
         when(UserServiceMock.getCurrentUser()).thenReturn(new User("rossjohnson@google.com", "yeet"));
-        String redirectUrl = "/redirect";
-        Boolean loggedin = AuthServlet.isUserLoggedIn(UserServiceMock);
+        when(UserServiceMock.createLoginURL("/redirect")).thenReturn("");
+        setFinalStatic(ServletUtil.class.getField("USER_SERVICE"), UserServiceMock);
         //Expected output
         String expected = "rossjohnson@google.com";
         
-        //Actual outpud
-        String actual = AuthServlet.getResponseBody(loggedin, redirectUrl, UserServiceMock);
-
+        //Execution
+        new AuthServlet().doGet(req, res);
         //Check if equal
-        Assert.assertEquals(expected, actual);
+        Assert.assertTrue(expected, stringWriter.getBuffer().toString().contains(expected));
     }
 
     @Test
-    public void UserServiceTestUserIsNotLoggedIn(){
+    public void UserServiceTestUserIsNotLoggedIn() throws Exception{
         //Setup
         UserService UserServiceMock = Mockito.mock(UserService.class);
         when(UserServiceMock.isUserLoggedIn()).thenReturn(false);
-        when(UserServiceMock.createLoginURL("redirect")).thenReturn("");
-        Boolean loggedin = AuthServlet.isUserLoggedIn(UserServiceMock);
+        when(UserServiceMock.createLoginURL("/redirect")).thenReturn("");
+        setFinalStatic(ServletUtil.class.getField("USER_SERVICE"), UserServiceMock);
         //Expected result
         String expected = "<center><a href=\"" + "\"><button style=\"background-color: cyan; border-radius:12px;\"><span style=\"color:white;\">Login</span></button/></a></center>";
         
-        //Actual result
-        String actual = AuthServlet.getResponseBody(loggedin,"redirect",UserServiceMock);
+        //Execution
+        new AuthServlet().doGet(req, res);
 
         //Check if equal
-        Assert.assertEquals(expected, actual);
+        Assert.assertTrue(expected, stringWriter.getBuffer().toString().contains(expected));
+    }
+
+    static void setFinalStatic(Field fieldToChange, Object newObjValue) throws Exception {
+        fieldToChange.setAccessible(true);
+
+        // remove final modifier from field
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(fieldToChange, fieldToChange.getModifiers() & ~Modifier.FINAL);
+
+        fieldToChange.set(null, newObjValue);
     }
 }
